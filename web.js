a3dc6d93da13c6f629ad07355bf35cdeec0e199e
Tszmei Ko <tmko888@hotmail.com>
2013-07-15 03:15:17 +0000

    Another update web.js

--- web.js
+++ web.js
@@ -1,13 +1,18 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
+
 var indexF = "./index.html";
+var fs = require('fs');  
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  var buf = new Buffer( fs.readFileSync(indexF) );
-  response.send(buf.toString);
+  //var buf = new Buffer( fs.readFileSync(indexF) );
+  //response.send(buf.toString);
+  var buf = new Buffer( fs.readFileSync(indexF, 'utf8' ) );
+  var outstr = buf.toString('utf8');
+  response.send(outstr);
+  
 });
 
 var port = process.env.PORT || 5000;
