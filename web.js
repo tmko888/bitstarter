8a62d474b0325e4132d51c2bbb8072e6eccd37d2
Tszmei Ko <tmko888@hotmail.com>
2013-07-15 02:36:19 +0000

    Another update web.js

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(buf.toString(fs.readFileSync(index.html)));
+  var buf = new Buffer( fs.readFileSync(index.html) );
+  response.send(buf.toString);
 });
 
 var port = process.env.PORT || 5000;
